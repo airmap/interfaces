4cde5af2dab440bc59e23b618c92a7454f20a79d
Thomas Voß <thomas@airmap.com>
2018-06-07 12:55:50 +0200

    [interfaces] Add first node.js example

---
+++ examples/nodejs/traffic/client.js
@@ -0,0 +1,50 @@
+// Path to the .proto that we are going to use for this example.
+var PROTO_PATH = __dirname + '/../../../airmap/grpc/traffic.proto';
+
+// Bootstrap our dependencies.
+var ggrpc = require('grpc');
+var pl = require('@grpc/proto-loader');
+
+// Load the gRPC interface definitions.
+var proto = pl.loadSync(PROTO_PATH, {});
+var pkg = ggrpc.loadPackageDefinition(proto);
+
+// Create a client for connecting to the collector.
+// Please replace with the appropriate URL and appropriate credentials.
+var client = new pkg.grpc.TrafficCollector('localhost:9090', ggrpc.credentials.createInsecure());
+var source = client.registerSource();
+
+source.on('data', function(ack) {
+  console.log('received ack from collector', ack);
+});
+
+source.on('end', function() {
+  console.log('source connection to collector ended');
+});
+
+source.on('error', function() {
+  console.log('source connection to collector entered error state');
+});
+
+// We have successfully registered as a source of traffic data now.
+// Call source.write(...) for all the incoming observations you 
+// want to dispatch to the collector.
+//
+// We will now register as a processor of telemetry data, too.
+var params = {}
+var processor = client.registerProcessor(params);
+
+processor.on('data', function(update) {
+  console.log('received a new observation from collector');
+});
+
+processor.on('end', function() {
+  console.log('processor connection to collector ended');
+});
+
+processor.on('error', function() {
+  console.log('processor connection to collector entered error state');
+});
+
+
+
