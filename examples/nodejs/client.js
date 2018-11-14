30eea5475f6611d2c13557eeb5616af2452e3989
Brad Ewing <brad@airmap.com>
2018-11-13 19:47:48 -0800

    flesh out observation

--- examples/nodejs/client.js
+++ examples/nodejs/client.js
@@ -9,7 +9,7 @@ var units_pb = require('./units_pb.js')
 
 // Create a client for connecting to the collector.
 // Please replace with the appropriate URL and appropriate credentials.
-var client = new trafficService.TrafficProviderClient('localhost:7090', grpc.credentials.createInsecure())
+var client = new trafficService.TrafficProviderClient('localhost:7080', grpc.credentials.createInsecure())
 var source = client.registerProvider()
 
 source.on('data', function(ack) {
@@ -24,54 +24,62 @@ source.on('error', function(e) {
   console.log('source connection to collector entered error state: ', e)
 })
 
-var update = new traffic_pb.Traffic.Update.FromProvider()
 
-var submitted = new google_protobuf_timestamp_pb.Timestamp()
-var observation = new traffic_pb.Traffic.Observation()
-var sensor = new traffic_pb.Traffic.Sensor()
-var identity = new traffic_pb.Traffic.Identity()
 
-var observedTime = new google_protobuf_timestamp_pb.Timestamp()
-var ttl = new google_protobuf_duration_pb.Duration()
-var position = new measurements_pb.Position()
-var coordinate = new measurements_pb.Coordinate2D()
-var course = new measurements_pb.Course()
-var velocity = new measurements_pb.Velocity()
-var orientation = new measurements_pb.Orientation()
+function dispatchUpdate() {
+    var update = new traffic_pb.Traffic.Update.FromProvider()
+    var submitted = new google_protobuf_timestamp_pb.Timestamp()
+    var observation = new traffic_pb.Traffic.Observation()
+    var sensor = new traffic_pb.Traffic.Sensor()
+    var identity = new traffic_pb.Traffic.Identity()
+    var observedTime = new google_protobuf_timestamp_pb.Timestamp()
+    var ttl = new google_protobuf_duration_pb.Duration()
+    var position = new measurements_pb.Position()
+    var coordinate = new measurements_pb.Coordinate2D()
+    var course = new measurements_pb.Course()
+    var velocity = new measurements_pb.Velocity()
+    var orientation = new measurements_pb.Orientation()
 
-var seconds = Math.round(new Date().getTime()/1000)
+    var seconds = Math.round(new Date().getTime()/1000)
 
-submitted.setSeconds(seconds+1)
-update.setSubmitted(submitted)
+    identity.setProviderId(new traffic_pb.Traffic.Identity.ProviderId('provider.id'))
 
-observedTime.setSeconds(seconds)
-ttl.setSeconds(1)
+    submitted.setSeconds(seconds+1)
+    update.setSubmitted(submitted)
 
-coordinate.setLatitude(new units_pb.Degrees(33.98635))
-coordinate.setLongitude(new units_pb.Degrees(-118.47639))
+    observedTime.setSeconds(seconds)
+    ttl.setSeconds(1)
 
-position.setCoordinate(coordinate)
-position.setAltitude(new units_pb.Meters(50))
+    coordinate.setLatitude(new units_pb.Degrees(33.98635))
+    coordinate.setLongitude(new units_pb.Degrees(-118.47639))
 
-course.setAngle(0)
+    position.setCoordinate(coordinate)
+    position.setAltitude(new units_pb.Meters(50))
 
-velocity.setX(new units_pb.MetersPerSecond(0))
-velocity.setY(new units_pb.MetersPerSecond(3.2))
-velocity.setZ(new units_pb.MetersPerSecond(0.1))
+    course.setAngle(0)
 
-orientation.setYaw(new units_pb.Degrees(42))
-orientation.setPitch(new units_pb.Degrees(42))
-orientation.setRoll(new units_pb.Degrees(42))
+    velocity.setX(new units_pb.MetersPerSecond(0))
+    velocity.setY(new units_pb.MetersPerSecond(3.2))
+    velocity.setZ(new units_pb.MetersPerSecond(0.1))
 
-observation.setSensor(sensor)
-observation.setIdentitiesList(identity)
-observation.setObserved(observedTime)
-observation.setTtl(ttl)
-observation.setPosition(position)
-observation.setCourse(course)
-observation.setVelocity(velocity)
-observation.setOrientation(orientation)
+    orientation.setYaw(new units_pb.Degrees(42))
+    orientation.setPitch(new units_pb.Degrees(42))
+    orientation.setRoll(new units_pb.Degrees(42))
 
-update.setObservationsList(observation)
+    observation.setSensor(sensor)
+    observation.setIdentitiesList(identity)
+    observation.setObserved(observedTime)
+    observation.setTtl(ttl)
+    observation.setPosition(position)
+    observation.setCourse(course)
+    observation.setVelocity(velocity)
+    observation.setOrientation(orientation)
 
-source.write(update)
+    update.setObservationsList(observation)
+    source.write(update)
+}
+
+setInterval(function() {
+    dispatchUpdate()
+}, 1000)
+
